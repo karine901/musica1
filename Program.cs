e1ccdab648abaaea3f7e6b250d61ccb7f8fa70a1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 21:29:58 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("muleque de vila - projota");
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine(@"
+Eu falei que era uma questão de tempo
+E tudo ia mudar, e eu lutei
+Vários me disseram que eu nunca ia chegar, duvidei
+Lembra da ladeira, meu?
+Toda sexta-feira meu melhor amigo é Deus e o segundo melhor sou eu
+
+
+Eu tanto quis, tanto fiz, tanto fui feliz
+Eu canto Xis, canto Péricles, canto Elis
+Torcedor do Santos, desse pão e circo eu também quis
+Não sei feliz, mas geral merece não ser infeliz
+
+Prosperei com suor do meu trabalho
+Me guardei, lutei sem buscar atalho
+E sem pisar em ninguém
+Sem roubar também, então sei
+Que hoje o meu nome é Foda e meu sobrenome é Pra Caralho
+
+Deus olhou pra mim, disse assim: Escuta, neguin
+Pegue esse caderno e escreve em cada folha até o fim
+Eu disse: Senhor, sou tão tímido, sinto mó pavor
+Só subir no palco a perna congelou
+
+Mas rodei o Brasil, CD na mochila foi 50 mil
+Mão em mão, na rodoviária passando mó frio
+Quem viu, viu, Curitiba, meu tesouro, foi estouro
+25 mil, tio, DVD de ouro
+
+Triunfo bombou, Leandro estourou, Michel prosperou
+Dei valor, só trabalhador, homens de valor
+Minha cor não me atrapalhou, só me abençoou
+Quem falou que era moda, hoje felizmente se calou");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Vai, vai lá, não tenha medo do pior
+Eu sei que tudo vai mudar
+Você vai transformar o mundo ao seu redor
+Mas não vacila, muleque de vila, muleque de vila, muleque de vila
+Não vacila, muleque de vila, muleque de vila, muleque de vila");
+Console.ResetColor();
+Console.WriteLine(@"
+Já fui vaiado, já fui humilhado, já fui atacado
+Fui xingado, ameaçado, nunca amedrontado
+Aplaudido, reverenciado, homenageado
+Premiado pelos homens, por Deus abençoado
+
+Avisa o Rony que hoje é nós, não tem show, tô sem voz
+Se o Danilo não colar, vou buscar de Cross
+Se o Marques chegar, grita o Magrão, liga, mó função
+Tem churrasco, sem fiasco, tira espinha do salão
+
+Já cantei com Mano Brown, com Edi Rock, com Helião
+Com D2, com MV, dei um abraço no Chorão
+Aprendi fazer freestyle no busão
+Hoje é o mesmo freestyle, só que a gente faz no fundo do avião
+
+E hoje eu acordei chorando porque eu me peguei pensando
+Será que lá de cima a minha véia segue me olhando?
+Será que se me olhando, ela ainda tá me escutando?
+Será que me escutando, ela ainda tá se orgulhando?
+
+Hoje tanto faz, putaria tá demais
+Mais ninguém se liga mais, mais ninguém respeita os pais
+Mas pra mim tanto faz porque ainda tem Racionais
+Pra quem quer um diferente, tem Oriente e Haikass
+
+Raps nacionais, rostos diferentes, mesmos ideais
+Salve, Sabota, e todo rap sem lorota
+Os mano gosta de ir no Twitter xingar o Projota
+Mas trai a mulher e não abraça a mãe, faz uma cota
+
+Desde os 16 tô aqui, outra vez, vou sorrir
+Vou cantar, vou seguir
+Vou tentar, conseguir
+Se quer falar mal, fala daí
+Mas meu público grita tão alto que já nem consigo te ouvir
+
+Olha lá o outdoor com o meu nome
+Me emocionar não me faz ser menos homem
+Se o diabo amassa o pão, você morre ou você come?
+Eu não morri e nem comi, eu fiz amizade com a fome");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Vai, vai lá, não tenha medo do pior
+Eu sei que tudo vai mudar
+Você vai transformar o mundo ao seu redor
+Mas não vacila, muleque de vila, muleque de vila, muleque de vila
+Não vacila, muleque de vila, muleque de vila, muleque de vila");
+Console.ResetColor();
